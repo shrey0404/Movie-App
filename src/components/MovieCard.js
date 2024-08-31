fcf506b10715545dbaffc03c0604900cd2863b01
shrey0404 <shreybhatnagar6410@gmail.com>
2024-08-31 07:16:56 +0530

    Set up basic UI with SearchBar component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <img src={movie.poster} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <p>Release Date: {movie.releaseDate}</p>
+    </div>
+  );
+};
+
+export default MovieCard;
